8d63f1bede57cc01c7ae13eca814e0c2d13e99fa
Twojoy <twojoy@protonmail.com>
2022-11-08 13:19:31 +0545

    Remove build config for testnet

--- contract/build_testnet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo ">> Building contract for testing"
-echo ">> This build is for deploying in testnet only\n\n"
-
-rustup target add wasm32-unknown-unknown
-cargo build --all --target wasm32-unknown-unknown --release --features testnet
